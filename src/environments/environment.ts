798cb58bb31a06909530a33f8c7a4143096a4ecc
Mohammed Essaid <mohammed.essaid@soprasteria.com>
2020-04-03 22:52:51 +0200

    fix gitignore and environement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyC42-g3wrzqjDMX3ZvMPJqkoVcp4pTiJ6A',
+    authDomain: 'tennispart-29bf2.firebaseapp.com',
+    databaseURL: 'https://tennispart-29bf2.firebaseio.com',
+    projectId: 'tennispart-29bf2',
+    storageBucket: 'tennispart-29bf2.appspot.com',
+    messagingSenderId: '180061192047',
+    appId: '1:180061192047:web:ce943526e2e4966d75f29a',
+    measurementId: 'G-95ZX3NNCBT'
+  }
 };
 
 /*
