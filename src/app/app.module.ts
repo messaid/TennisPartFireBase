11dbb18fa473cddf263054102d49e4d9d9bd5319
Mohammed Essaid <mohammed.essaid@soprasteria.com>
2020-04-04 21:28:15 +0200

    set angular fire to 5 1 1 instead of

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './views/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialFeaturesModule } from './material-features.module';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    NgxAuthFirebaseUIModule.forRoot(environment.firebaseConfig),
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialFeaturesModule
