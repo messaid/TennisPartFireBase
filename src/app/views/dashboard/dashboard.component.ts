12b341a96dd160ed83020b951d3e5dc7c2effe27
Mohammed Essaid <mohammed.essaid@soprasteria.com>
2020-04-14 16:53:55 +0200

    restyle logout button

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -4,9 +4,10 @@ import { IUserState } from './../../store/state/user.state';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LinkMenuItem } from 'ngx-auth-firebaseui';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 import { AuthService } from 'src/app/service/auth.service';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,20 +15,31 @@ import { AuthService } from 'src/app/service/auth.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  //public userStatus$ = this.authService.userStatusChanges.asObservable();
   public userStatus$: Observable<User>;
+  private userInitial = new BehaviorSubject<string>('');
+  public userInitial$ = this.userInitial.asObservable();
   public userDisplayName$: Observable<string>;
   constructor(private authService: AuthService,
               private storeUser: Store<{ user: IUserState }>) {
-                this.userStatus$ = this.storeUser.pipe(select(selectUser));
-                this.userDisplayName$ = this.storeUser.pipe(select(selectUserDisplayName));
+    this.userDisplayName$ = this.storeUser.pipe(select(selectUserDisplayName));
+    this.userStatus$ = this.storeUser.pipe(select(selectUser));
+    this.userDisplayName$.subscribe(data => {
+      if (!isNullOrUndefined(data)) {
+        let initials = '';
+        if (data.split(' ').length > 1) {
+          initials = data.split(' ')[0].charAt(0) + data.split(' ')[1].charAt(0);
+        } else {
+          initials = data.split(' ')[0].charAt(0);
+        }
+        this.userInitial.next(initials);
+      }
+    });
   }
 
   ngOnInit() {
   }
 
-  logout(){
+  logout() {
     this.authService.logOut();
   }
 
